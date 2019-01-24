3d43e77987fad7b798294de9fa640285e9b77a9d
alex.liu <1074199421@qq.com>
2019-01-24 10:16:55 +0800

    【优化】新增阿里云环境

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,12 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // 阿里云环境
-  BASE_API: '"http://101.132.163.120:8090"'
+  // 邵阿里云环境
+  // BASE_API: '"http://101.132.163.120:8090"'
   // 本地环境
   // BASE_API: '"http://localhost:8090"'
   // 腾讯云环境
   // BASE_API: '"http://139.199.82.83:8090"'
+  // 阿里云环境
+  BASE_API: '"http://47.100.27.31:8090"'
 }
